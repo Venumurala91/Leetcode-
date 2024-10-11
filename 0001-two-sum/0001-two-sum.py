e67cec36fe6298d708eca38f731d013881828d1f
Venumurala91 <muralavenu24@gmail.com>
2024-10-11 22:03:00 +0530

    Time: 59 ms (63.53%), Space: 17.9 MB (11.99%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,45 +1,11 @@
 class Solution:
-    def twoSum(self, arr: List[int], target: int) -> List[int]:
-        
-
-
-        c={}
-        for i in range(len(arr)):
-            c[i]=arr[i]
-
-        arr.sort()
-        i=0
-        j=len(arr)-1
-
-        if len(arr)==2 and arr[0]+arr[1]==target:
-            return [0,1]
-
-        while i<j:
-            if arr[i]+arr[j]==target:
-                break
-            elif arr[i]+arr[j]>target:
-                j-=1
-            else:
-                i+=1
-        
-        f=True
-        for k in range(len(arr)):
-            if c[k]==arr[i] and f:
-                p=k
-                f=False
-            elif c[k]==arr[j]:
-                q=k
-        
-        return p,q
-
-
-        
-
-
-
-
-
-
-
-        
-        
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]]
+        # If no valid pair is found, return an empty list
+        return []
