35817008ad5e7dd59c9bc8d410ab7b11fa8f35fe
Venumurala91 <muralavenu24@gmail.com>
2024-12-09 10:30:30 +0530

    Time: 14 ms (31.11%), Space: 34.5 MB (7.28%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        dic={}
+        for i in range(len(nums)):
+            if nums[i] in dic:
+                return True
+            else:
+                dic[nums[i]]=1
+
+        
+        return False
