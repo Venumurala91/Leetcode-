dd3d17b66ad2c1b0ca2c06d31b2e762e33333f5f
Venumurala91 <muralavenu24@gmail.com>
2024-12-11 19:05:07 +0530

    Time: 9 ms (21.83%), Space: 17.2 MB (17.38%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,34 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dic={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+
+        if len(s)==1:
+            return dic[s[0]]
+
+        SUM=0
+        visited=False
+        for i in range(len(s)-1):
+            if dic[s[i+1]]>dic[s[i]]:
+                SUM+=int(dic[s[i+1]])-int(dic[s[i]])
+                visited=True
+
+            elif not visited:
+                SUM+=int(dic[s[i]])
+            else:
+                visited=False
+
+
+
+        
+       
+        if i<len(s) and dic[s[i+1]]==dic[s[i]]:
+            SUM+=int(dic[s[i]])
+        
+        elif dic[s[i+1]]<dic[s[i]]:
+            SUM+=int(dic[s[i+1]])
+
+        
+
+
+        return SUM 
+
