14ae831defd7aff45c2de4cfcd684721202f290d
Venu <muralavenu24@gmail.com>
2025-01-30 18:22:40 +0530

    Time: 0 ms (100%), Space: 18.9 MB (39.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,9 +3,12 @@ class Solution:
         hashmap = {}
         for i in range(len(nums)):
             hashmap[nums[i]] = i
+
         for i in range(len(nums)):
             complement = target - nums[i]
             if complement in hashmap and hashmap[complement] != i:
                 return [i, hashmap[complement]]
         # If no valid pair is found, return an empty list
         return []
+
+        
